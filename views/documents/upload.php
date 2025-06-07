d065f27e54b7146c99eb0f39cb857711055bd863
Militsa Toncheva <mili.tonvheva@gmail.com>
2025-06-07 18:27:57 +0300

    delete dirs /views and /config

--- views/documents/upload.php
+++
@@ -1,7 +0,0 @@
-<select name="category_id" id="category_id">
-    <?php foreach ($categories as $category): ?>
-        <option value="<?= $category['id'] ?>">
-            <?= htmlspecialchars($category['name']) ?>
-        </option>
-    <?php endforeach; ?>
-</select>
