d0504afa7be1196d4d453d73abe4699c20ef024d
Hari <hari.hristov.03@gmail.com>
2025-06-07 14:54:49 +0300

    Fixed the DocumentController

--- app/controllers/DocumentController.php
+++ app/controllers/DocumentController.php
@@ -3,7 +3,6 @@
 
 require_once __DIR__ . '/../models/Document.php';
 require_once __DIR__ . '/../models/Category.php';
-require_once __DIR__ . '/../models/AccessLog.php';
 require_once __DIR__ . '/../core/Helper.php';
 
 class DocumentController
@@ -19,33 +18,30 @@ class DocumentController
     public function store()
     {
         if (!isset($_FILES['document'])) {
-            die('Няма подаден файл.');
+            die('Няма качен файл.');
         }
 
-        $uploadDir = __DIR__ . '/../../storage/';
-        $filename = basename($_FILES['document']['name']);
-        $destination = $uploadDir . $filename;
+        $filename = Helper::sanitizeFilename($_FILES['document']['name']);
+        $destination = UPLOAD_PATH . '/' . $filename;
 
         if (move_uploaded_file($_FILES['document']['tmp_name'], $destination)) {
             $docModel = new Document();
             $accessCode = Helper::generateCode(10);
 
             $docId = $docModel->create($filename, $_POST['category_id'], $accessCode);
-            
-            // логване на качването
-            $log = new AccessLog();
-            $log->log($docId, 'uploaded');
 
-            echo "Документът е качен успешно! Входящ номер: {$docId}, Достъп: {$accessCode}";
+            echo "Документът е качен успешно! Входящ номер: {$docId}, Код за достъп: {$accessCode}";
         } else {
-            echo "Грешка при качване на документа.";
+            echo "Грешка при качване.";
         }
     }
 
     public function status()
     {
         $code = $_GET['code'] ?? '';
-        if (!$code) die('Невалиден код');
+        if (!$code) {
+            die('Невалиден код.');
+        }
 
         $docModel = new Document();
         $document = $docModel->getByAccessCode($code);
