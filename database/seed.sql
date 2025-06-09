d2eaece8e9513e34fe4535a49e0fb204f7193326
polinaDoncheva <polidoncheva03@gmail.com>
2025-06-09 22:56:50 +0300

    Add query for 3 user profiles

--- database/seed.sql
+++ database/seed.sql
@@ -1,6 +1,19 @@
+-- Вмъкване на категории
 INSERT INTO categories (name) VALUES
 ('Отдел Студенти'),
 ('Учебен отдел – Магистри'),
 ('Кандидат-студенти'),
 ('Сесия'),
 ('Без категория');
+
+-- Вмъкване на общ потребител (role: user)
+INSERT INTO users (username, password, full_name, role)
+VALUES ('student', 'student', 'Общ потребител', 'user');
+
+-- Вмъкване на отговарящ потребител (role: responsible)
+INSERT INTO users (username, password, full_name, role)
+VALUES ('teacher', 'teacher', 'Отговарящ потребител', 'responsible');
+
+-- Вмъкване на администратор (role: admin)
+INSERT INTO users (username, password, full_name, role)
+VALUES ('admin', 'admin', 'Администратор', 'admin');
