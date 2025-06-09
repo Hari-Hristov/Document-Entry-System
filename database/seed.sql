30e473d1f5abcfb1e91437e846ef5af5736480ba
Militsa Toncheva <mili.tonvheva@gmail.com>
2025-06-09 23:39:52 +0300

    adding users in user table

--- database/seed.sql
+++ database/seed.sql
@@ -4,3 +4,15 @@ INSERT INTO categories (name) VALUES
 ('Кандидат-студенти'),
 ('Сесия'),
 ('Без категория');
+
+-- Вмъкване на общ потребител (role: user)
+INSERT INTO users (username, password, full_name, role)
+VALUES ('student', 'student', 'Общ потребител', 'user');
+
+-- Вмъкване на отговарящ потребител (role: responsible)
+INSERT INTO users (username, password, full_name, role)
+VALUES ('teacher', 'teacher', 'Отговарящ потребител', 'responsible');
+
+-- Вмъкване на администратор (role: admin)
+INSERT INTO users (username, password, full_name, role)
+VALUES ('admin', 'admin', 'Администратор', 'admin');
